37b3f82997f6c1eba38887aa83e11f6689b29421
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-17 20:50:37 +0300

    put and delete http request

--- app.js
+++ app.js
@@ -44,5 +44,46 @@ postButton.addEventListener('click', function () {
         alert(JSON.stringify(data));
         fetchEmployees();
     })
+});
 
+// put request
+let putButton = document.querySelector(#'put-btn');
+putButton.addEventListener('click', function () {
+    let empID = '_abcde',
+        let employee = {
+            id: empID,
+            first_name: 'john',
+            last_name: 'wilson',
+            email: 'johnwilson@gmail.com',
+            gender: 'male',
+            ip_address: '255.255.255.255'
+        }
+    let url = `${serverURL}/employee/${empID}`;
+    let http = new brainhttp();
+    http.put(url, employee, (data) => {
+        alert(JSON.stringify(data));
+        fetchEmployees();
+    })
+
+});
+
+// delete button
+let deleteButton = document.querySelector('#delete-btn');
+deleteButton.addEventListener('click', function () {
+    let employeeID = `_vwxyz`
+    let url = `${serverURL}/employees`;
+    let http = new brainhttp();
+    http.delete(url, (data) => {
+        alert(JSON.stringify(data));
+        fetchEmployees();
+    })
 })
+
+
+
+
+
+
+
+
+
